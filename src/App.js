3cf8dbe28b23ffa1bacf62b4727dcee78d1aea98
Derek Witteck <drwitteck@gmail.com>
2018-08-30 21:18:07 -0400

    Used props and state to update data in Person.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import Person from './Person/Person.js'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Derek", age: 36 },
+      { name: "Emily", age: 31 },
+      { name: "Shannon", age: 40 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
-        <Person />
+        <button>Switch Name</button>
+        <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age }> My hobby: tech </Person>
+        <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age }/>
+        <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age }/>        
       </div>
     );
   }
